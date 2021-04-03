2aa965d833b8b201b39306917c9c53107980d8f0
Yashraj Singh <yyyyashssssingh@gmail.com>
2021-04-03 16:01:07 +0530

    Added Button Sounds

---
+++ Assets/Scripts/buttonFX.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonFX : MonoBehaviour {
+    
+	public AudioSource myFx;
+	public AudioClip hoverFx;
+	public AudioClip clickFx;
+
+	public void HoverSOund() {
+		myFx.PlayOneShot(hoverFx);
+	}
+
+	public void ClickSound() {
+		myFx.PlayOneShot(clickFx);
+	}
+
+}
