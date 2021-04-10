62476c5f1c40d573d646e30529c6f801d7de7687
absseth1202 <absseth1202@gmail.com>
2021-04-10 18:40:53 +0530

    Added player option panel

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,8 @@ public class MainMenu : MonoBehaviour
 {
    public void PlayGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);  
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Hey !! It's me");
     }
 
     public void QuitGame()
