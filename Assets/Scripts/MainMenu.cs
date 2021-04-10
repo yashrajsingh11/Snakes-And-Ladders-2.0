cff0764ad5ea9757dae35fa7c2a438302cccc71e
vijayshankarrealdeal <vijaygaya056@gmail.com>
2021-04-10 19:17:34 +0530

    Changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Firebase;
+using Firebase.Auth;
 
 public class MainMenu : MonoBehaviour
 {
    public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Auth auth = new Auth();
+        auth.loginAnno();
         Debug.Log("Hey !! It's me");
     }
 
@@ -17,3 +21,32 @@ public class MainMenu : MonoBehaviour
         Application.Quit();
     }
 }
+
+public class Auth
+{
+    Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+   
+    
+
+
+    public void loginAnno()
+    {
+        auth.SignInAnonymouslyAsync().ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInAnonymouslyAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+}
+
