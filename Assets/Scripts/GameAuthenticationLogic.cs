01a2a5e469fc13a4bda68946f1860dec8b64a872
Yashraj Singh <yyyyashssssingh@gmail.com>
2021-04-13 15:31:30 +0530

    .

--- Assets/Scripts/GameAuthenticationLogic.cs
+++ Assets/Scripts/GameAuthenticationLogic.cs
@@ -7,16 +7,15 @@ using Firebase.Firestore;
 using System;
 using UnityEngine.SceneManagement;
 
-public class GameAuthenticationLogic : MonoBehaviour
-{
+public class GameAuthenticationLogic : MonoBehaviour {
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
 
     }
 
     public bool isConnectedToServer = false;
-    public bool allPlayerPresent = false;
+    public bool isEveryonePresent = false;
+    public bool isInRoom = false;
     public string useridXXXX;
     public string player2iD = "";
 
@@ -24,97 +23,91 @@ public class GameAuthenticationLogic : MonoBehaviour
 
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
 
-        if (isConnectedToServer == true && allPlayerPresent == true)
-        {
-            SceneManager.LoadSceneAsync("SampleScene");
-            Debug.Log("Hey Its Me");
+        if (isConnectedToServer == true && isEveryonePresent == true && isInRoom == true) {
             isConnectedToServer = false;
-            allPlayerPresent = false;  
+            isEveryonePresent = false; 
+            isInRoom = false; 
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            Debug.Log("Game Start");
         }
-        if(isConnectedToServer == true && allPlayerPresent == false) {
-        FirebaseFirestore _refrence = FirebaseFirestore.DefaultInstance;
-        for (int i = 0; i < userDataAll.Count; i++) {
-          //Player 1 and Player 2
-          if (userDataAll[i].isOnline && userDataAll[i].uid != useridXXXX && userDataAll.Count > 1 && isConnectedToServer == true) {
-            player2iD = userDataAll[i].uid;
-            Debug.Log(useridXXXX);
-                  Debug.Log(player2iD);
-                  UserModelClassX userClassModel = new UserModelClassX(useridXXXX, true);
-                  if(!string.IsNullOrEmpty(player2iD)) {
-                  _refrence.Collection("Room").Document(useridXXXX + player2iD).SetAsync(
-   userClassModel.toJsonRoomCreateXX(useridXXXX, player2iD)
 
-                   ).ContinueWith(task =>
-               {
-                   Debug.Log("Hello Rooms");
-                   allPlayerPresent = true;
-               });
-            break;
-          }
-          } else {
-            Debug.Log("Ruk jao bhai");
-          }
+        if (isConnectedToServer == true && isEveryonePresent == false) {
+            FirebaseFirestore _refrence = FirebaseFirestore.DefaultInstance;
+            for (int i = 0; i < userDataAll.Count; i++) {
+            //Player 1 and Player 2
+                abc();
+                if (userDataAll[i].isOnline && userDataAll[i].uid != useridXXXX && userDataAll.Count > 1) {
+                    player2iD = userDataAll[i].uid;
+                    Debug.Log(useridXXXX);
+                    Debug.Log(player2iD);
+                    UserModelClassX userClassModel = new UserModelClassX(useridXXXX, true);
+                    _refrence.Collection("Room").Document(useridXXXX + player2iD).SetAsync(userClassModel.toJsonRoomCreateXX(useridXXXX, player2iD)
+                    ).ContinueWith(task => {
+                        Debug.Log("Entering room");
+                        isInRoom = true;
+                    });
+                        isEveryonePresent = true;
+                    break;
+                } else {
+                    Debug.Log("Waiting for second user");
+                }
+            }
         }
-      }
     }
 
-    public void loginAnno()
-    {
+    public void abc() {
+
+        FirebaseFirestore _refrence = FirebaseFirestore.DefaultInstance;
+        if(userDataAll.Count <= 1) {
+            _refrence.Collection("Users").GetSnapshotAsync().ContinueWith(task => {
+                QuerySnapshot snapshot = task.Result;
+                foreach (DocumentSnapshot document in snapshot.Documents) {
+                    Dictionary<string, object> resultsHere = document.ToDictionary();
+                    ///deserizz the data code
+                    UserDeserializeData dsData = new UserDeserializeData {
+                        uid = resultsHere["uid"].ToString(),
+                        isOnline = bool.Parse(resultsHere["isOnline"].ToString()),
+                        dateTime = resultsHere["dateTime"].ToString(),
+                    };
+                    userDataAll.Add(dsData);
+                };
+            });
+        }
+        Debug.Log("abc ran");
+    }
+
+    public void loginAnno() {
 
         Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
-        auth.SignInAnonymouslyAsync().ContinueWith(task =>
-        {
-            if (task.IsCanceled)
-            {
+        auth.SignInAnonymouslyAsync().ContinueWith(task => {
+            if (task.IsCanceled) {
                 Debug.LogError("SignInAnonymouslyAsync was canceled.");
                 return;
             }
-            if (task.IsFaulted)
-            {
+            if (task.IsFaulted) {
                 Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
                 return;
             }
             Firebase.Auth.FirebaseUser newUser = task.Result;
             useridXXXX = newUser.UserId;
-            // Debug.Log(useridXXXX);
             addUserToDatabase();
         });
     }
 
-    public void addUserToDatabase()
-    {
+    public void addUserToDatabase() {
 
         FirebaseFirestore _refrence = FirebaseFirestore.DefaultInstance;
         UserModelClassX userClassModel = new UserModelClassX(useridXXXX, true);
         _refrence.Collection("Users").Document(useridXXXX)
-           .SetAsync(userClassModel.toJsonX())
-          .ContinueWith(task =>
-          {
-              Debug.Log("Mast");
-               ///NewCodeLine....
-               _refrence.Collection("Users").GetSnapshotAsync().ContinueWith(task =>
-              {
-                  QuerySnapshot snapshot = task.Result;
-                  foreach (DocumentSnapshot document in snapshot.Documents)
-                  {
-                      Dictionary<string, object> resultsHere = document.ToDictionary();
-                      ///deserizz the data code
-                      UserDeserializeData dsData = new UserDeserializeData
-                      {
-                          uid = resultsHere["uid"].ToString(),
-                          isOnline = bool.Parse(resultsHere["isOnline"].ToString()),
-                          dateTime = resultsHere["dateTime"].ToString(),
-                      };
-                      userDataAll.Add(dsData);
-                  };
-                 isConnectedToServer = true;
-              });
-          });
+            .SetAsync(userClassModel.toJsonX())
+            .ContinueWith(task => {
+                Debug.Log("Connected To Server");
+                isConnectedToServer = true;
+                abc();
+        });
     }
-
 }
 
 
